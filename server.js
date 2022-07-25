0459094499aca5eae931a8378618b6ffed575a07
dikiamust <agungmustaqimdiki@gmail.com>
2022-07-25 21:02:34 +0700

    add graphql

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express')
-const env = require('dotenv').config()
+const env = require('dotenv').config();
+const userData = require('./MOCK_DATA.json');
+const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLScalarType, GraphQLInt, GraphQLString } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
 
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+
+const RootQuery = new GraphQLObjectType()
+const Mutation = 'mutation';
+
+const schema = new GraphQLSchema({query:  RootQuery, mutation: Mutation})
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}))
 
 const port = process.env.PORT || 3300
 
